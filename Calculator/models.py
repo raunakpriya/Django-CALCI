dc634bb14ca227bfa4eb56a9be13d13f2ac2d068
Raunak Priya <raunakpriya@gmail.com>
2020-01-23 16:33:59 +0530

    Update models.py

--- Calculator/models.py
+++ Calculator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    operator=models.CharField(max_length=1)
+    num1=models.IntegerField()
+    num2=models.IntegerField()
+    res=models.IntegerField()
+
+def __str__(self):
+        return self.res

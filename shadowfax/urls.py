56d769e9fb80cff1c6d565748f958698e2ff62d4
Raunak Priya <raunakpriya@gmail.com>
2020-01-24 10:56:51 +0530

    Update urls.py

--- shadowfax/urls.py
+++ shadowfax/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib import admin
-#from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken.views import obtain_auth_token
 
-#from rest_framework.authtoken import views
+from rest_framework.authtoken import views
 from Calculator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),path('Calculator',views.Calculator.as_view()),
-    #path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     
 ]
